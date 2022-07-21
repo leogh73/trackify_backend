9aeee66a992267f48f068518d66025f91184f766
LEO <leocuevas73@gmail.com>
2022-07-21 10:57:56 -0300

    new test code

--- api/routes/users_routes.js
+++ api/routes/users_routes.js
@@ -1,6 +1,8 @@
 import express from 'express';
 export const router = express.Router();
-import puppeteer from 'puppeteer';
+// import puppeteer from 'puppeteer';
+import chromium from 'chrome-aws-lambda';
+import playwright from 'playwright-core';
 import user from '../controllers/users_controllers.js';
 
 router.post('/initialize', user.initialize);
@@ -11,11 +13,17 @@ router.post('/request', user.serviceRequest);
 
 router.get('/test', async (req, res) => {
 	try {
-		const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
-		const page = await browser.newPage();
+		const browser = await playwright.chromium.launch({
+			args: [...chromium.args, '--font-render-hinting=none'],
+			executablePath: await chromium.executablePath,
+		});
+		const context = await browser.newContext();
+		const page = await context.newPage();
 
-		await page.goto(`${process.env.CLICOH_API_URL1}`);
-		await page.waitForSelector("input[name='codigo']");
+		await page.goto(`${process.env.CLICOH_API_URL1}`, {
+			waitUntil: 'load',
+		});
+
 		await page.type("input[name='codigo']", 'HWUIN94250');
 		let data = await (
 			await Promise.all([
@@ -37,6 +45,35 @@ router.get('/test', async (req, res) => {
 		return res.status(500).send({ 'Server Error': `${error}` });
 	}
 });
+
+// router.get('/test', async (req, res) => {
+// 	try {
+// const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+// 		const page = await browser.newPage();
+
+// 		await page.goto(`${process.env.CLICOH_API_URL1}`);
+// 		await page.waitForSelector("input[name='codigo']");
+// 		await page.type("input[name='codigo']", 'HWUIN94250');
+// 		let data = await (
+// 			await Promise.all([
+// 				page.waitForResponse(
+// 					(response) =>
+// 						response.url() === `${process.env.CLICOH_API_URL2}` &&
+// 						response.request().method() === 'POST',
+// 				),
+// 				page.click('.fa.fa-search'),
+// 			])
+// 		)[0].json();
+// 		await browser.close();
+// 		res.json({
+// 			status: 200,
+// 			message: data,
+// 		});
+// 	} catch (error) {
+// 		console.error(error);
+// 		return res.status(500).send({ 'Server Error': `${error}` });
+// 	}
+// });
 
 // router.get('/test', async (req, res) => {
 // 	try {
