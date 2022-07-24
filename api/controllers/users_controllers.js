ae8f5a1748a17459bf1f77d0cb1c3eaef4b5c45b
leogh73 <leocuevas73@gmail.com>
2022-07-24 10:03:09 -0300

    minor fix

--- api/controllers/users_controllers.js
+++ api/controllers/users_controllers.js
@@ -53,7 +53,6 @@ const trackingAction = async (req, res) => {
 const sincronize = async (req, res) => {
 	const { userId, token, lastEvents, currentDate, driveLoggedIn } = req.body;
 	try {
-		// let response = {};
 		let response = {};
 		let user = await update(userId, token);
 		if (user.error) {
