9e5bd256ae2e7d53c99e2daeedf074daa8aeda49
leogh73 <leocuevas73@gmail.com>
2022-07-29 14:49:29 -0300

    test code added

--- api/modules/cron.js
+++ api/modules/cron.js
@@ -1,6 +1,17 @@
 import { CronJob } from 'cron';
 import trackings from '../controllers/trackings_controllers.js';
 
-export let job = new CronJob('0 0 */2 * * *', async () => await trackings.checkCycle());
+export let job = new CronJob(
+	'0 */30 * * * *',
+	async () => await trackings.checkCycle(),
+	async () =>
+		await Models.storeLog({
+			actionName: 'check cycle',
+			actionDetail: 'cycle completed',
+			errorMessage: '-',
+			date: luxon.getDate(),
+			time: luxon.getTime(),
+		}),
+);
 
 job.start();
