dd299550827af11e9869a31ffbac55484a70c258
Leonardo Cuevas <leocuevas73@gmail.com>
2023-05-16 16:41:15 -0300

    activation endpoint

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import dotenv from 'dotenv/config';
 import { router as user } from './src/routes/users_routes.js';
 import { router as mercadoLibre } from './src/routes/mercadolibre_routes.js';
 import { router as google } from './src/routes/google_routes.js';
+import got from 'got';
 
 app.use(compression());
 app.setMaxListeners(20);
@@ -15,6 +16,11 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
+
+app.use(async (req, res, next) => {
+	await got(process.env.PLAYWRIGHT_API_URL);
+	next();
+});
 
 app.use('/api/user', user);
 app.use('/api/google', google);
