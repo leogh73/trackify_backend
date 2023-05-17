b4596dcc59a5c8a52b3a993967c994d5be0165b6
Leonardo Cuevas <leocuevas73@gmail.com>
2023-05-17 12:00:26 -0300

    dependency changed

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
 import dotenv from 'dotenv/config';
-import vars from './src/modules/crypto-js.js';
 
 import { router as user } from './src/routes/users_routes.js';
 import { router as mercadoLibre } from './src/routes/mercadolibre_routes.js';
 import { router as google } from './src/routes/google_routes.js';
-import got from 'got';
 
 app.use(compression());
 app.setMaxListeners(20);
@@ -17,11 +15,6 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
-
-app.use(async (req, res, next) => {
-	await got(vars.PLAYWRIGHT_API_URL);
-	next();
-});
 
 app.use('/api/user', user);
 app.use('/api/google', google);
