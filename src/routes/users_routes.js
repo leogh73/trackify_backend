c1c2b5c31b120ed3053f94ae154839e40cd75824
Leonardo Cuevas <leocuevas73@gmail.com>
2023-05-24 15:47:08 -0300

    add-delete blocked

--- src/routes/users_routes.js
+++ src/routes/users_routes.js
@@ -5,7 +5,7 @@ import trackings from '../controllers/trackings_controllers.js';
 import user from '../controllers/users_controllers.js';
 
 router.post('/initialize', user.initialize);
-// router.post('/:userId/:action', user.trackingAction);
+router.post('/:userId/:action', user.trackingAction);
 router.post('/sincronize', user.sincronize);
 router.post('/check', user.check);
 router.post('/contact', user.contactForm);
