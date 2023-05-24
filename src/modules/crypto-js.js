e2e8288f4f0f1b36af7e199f27deafffa3819428
Leonardo Cuevas <leocuevas73@gmail.com>
2023-05-24 15:43:35 -0300

    add-delete option commented

--- src/modules/crypto-js.js
+++ src/modules/crypto-js.js
@@ -10,5 +10,4 @@ import { encrypted } from '../../env-var.enc.js';
 
 let bytes = CryptoJS.AES.decrypt(encrypted, process.env.SERVICE_ENCRYPTION_KEY);
 let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-
 export default decryptedData;
