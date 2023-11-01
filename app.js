8304e8e36174e28fd98f9ead8b146f0c2466c3f1
Leonardo Cuevas <leocuevas73@gmail.com>
2023-11-01 18:49:28 -0300

    minor fix

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ import { router as user } from './src/routes/users_routes.js';
 import { router as mercadoLibre } from './src/routes/mercado_libre_routes.js';
 import { router as googleDrive } from './src/routes/google_drive_routes.js';
 import { router as cronJobs } from './src/routes/cron_jobs_routes.js';
-import { router as dev } from './src/routes/dev_routes.js';
+// import { router as dev } from './src/routes/dev_routes.js';
 
 app.use('/api/user', user);
 app.use('/api/googledrive', googleDrive);
