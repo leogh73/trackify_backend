97e974218242947182c69bbf690aed2be28503fd
Leonardo Cuevas <leocuevas73@gmail.com>
2023-11-01 22:56:48 -0300

    database update fix

--- src/controllers/trackings_controllers.js
+++ src/controllers/trackings_controllers.js
@@ -162,7 +162,7 @@ async function updateDatabase(response, tracking, completedStatus) {
 			},
 		),
 	);
-	if (tracking.result.moreData) {
+	if (tracking.result.moreData && response.result.moreData) {
 		for (let element of response.result.moreData) {
 			databaseUpdates.push(
 				db.Tracking.findOneAndUpdate(
