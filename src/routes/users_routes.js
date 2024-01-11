6e0ae05c885d67c2206a9560156006934d4bc0ad
Leonardo Cuevas <leocuevas73@gmail.com>
2024-01-11 10:49:51 -0300

    user routes disabled, thanks Google AdMob

--- src/routes/users_routes.js
+++ src/routes/users_routes.js
@@ -3,7 +3,7 @@ export const router = express.Router();
 import user from '../controllers/users_controllers.js';
 
 router.post('/initialize', user.initialize);
-router.post('/:userId/:action', user.trackingAction);
-router.post('/syncronize', user.syncronize);
-router.post('/check', user.check);
-router.post('/contact', user.contactForm);
+// router.post('/:userId/:action', user.trackingAction);
+// router.post('/syncronize', user.syncronize);
+// router.post('/check', user.check);
+// router.post('/contact', user.contactForm);
