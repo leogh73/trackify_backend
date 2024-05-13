cb6b6a82522f6bd5fe78839d359940f9381e7461
Leonardo Cuevas <leocuevas73@gmail.com>
2024-05-13 16:53:07 -0300

    cruz del sur fixed

--- src/services/cruz_del_sur.js
+++ src/services/cruz_del_sur.js
@@ -1,51 +1,73 @@
 import got from 'got';
 import vars from '../modules/crypto-js.js';
 import services from './_services.js';
+import { load } from 'cheerio';
 
 async function check(code, lastEvent) {
-	let consult = await got.post(`${vars.CRUZ_DEL_SUR_API_URL}`, {
-		form: { nic: code, f: 'check_nip' },
+	let consult = await got(`${vars.CRUZ_DEL_SUR_API_URL}${code}`);
+	const $ = load(consult.body);
+
+	let baseTexts = [];
+	$(
+		'body > div.content_wrap > div.row.row_full.row_space_mobile > div > div> form > div > div > label',
+	).each(function () {
+		baseTexts.push($(this).text());
 	});
-	let result = JSON.parse(consult.body);
 
-	const getTimestampData = (ts) => {
-		let timestamp = new Date(ts);
-		let date = `${timestamp.getDate().toString().padStart(2, 0)}/${(timestamp.getMonth() + 1)
-			.toString()
-			.padStart(2, 0)}/${timestamp.getFullYear()}`;
+	if (baseTexts.length === 6) return { error: 'No data' };
 
-		let time = `${timestamp.getHours().toString().padStart(2, 0)}:${timestamp
-			.getMinutes()
-			.toString()
-			.padStart(2, 0)}`;
-		return { date, time };
-	};
+	let baseInputsTexts = [];
+	$(
+		'body > div.content_wrap > div.row.row_full.row_space_mobile > div > div > form > div > div> input',
+	).each(function () {
+		baseInputsTexts.push($(this).val());
+	});
 
-	let eventsList = result.Cuerpo.map((event) => {
-		return {
-			date: getTimestampData(event.Fecha).date,
-			time: getTimestampData(event.Fecha).time,
-			branch: event.NombreDeSucursal.trim(),
-			detail: event.Titulo,
-		};
-	}).reverse();
+	let eventsInputTexts = [];
+	$(
+		'body > div.content_wrap > div.row.row_full.row_space_mobile > div > div > form > div > div > div > input',
+	).each(function () {
+		eventsInputTexts.push($(this).val());
+	});
 
-	let {
-		Fecha,
-		DestinoNombre,
-		DestinoSucursal,
-		DestinoLocalidad,
-		RemitenteNombre,
-		RemitenteSucursal,
-	} = result.Cabecera;
 	let otherData = {
-		'Fecha de carga': getTimestampData(Fecha).date,
-		Destinatario: DestinoNombre.trim(),
-		'Sucursal de destino': DestinoSucursal.trim(),
-		Remitente: RemitenteNombre.trim(),
-		'Sucursal de origen': RemitenteSucursal.trim(),
-		Destino: DestinoLocalidad.trim(),
+		'Fecha de carga': `${eventsInputTexts[0]}/${eventsInputTexts[1]}/${eventsInputTexts[2]}`,
+		Destinatario: baseInputsTexts[0],
+		'Sucursal de destino': baseInputsTexts[2].split(' *')[0],
+		Remitente: baseInputsTexts[1].trim(),
+		'Sucursal de origen': baseInputsTexts[3],
+		Destino: baseInputsTexts[4],
 	};
+
+	let eventsData2 = [];
+	for (let i = 3; i < eventsInputTexts.length; i += 2) {
+		eventsData2.push(eventsInputTexts.slice(i, i + 2));
+	}
+
+	baseTexts.splice(0, 6);
+	let indexesList = [];
+	baseTexts.forEach((event, index) => {
+		if (event.includes('-')) indexesList.push(index);
+	});
+	let eventsData1 = [];
+	indexesList.forEach((i, index) => {
+		eventsData1.push(baseTexts.slice(i, indexesList[index + 1]));
+	});
+
+	let eventsList = eventsData1
+		.map((event, i) => {
+			let extraDetail;
+			if (event[2]) extraDetail = event[2].split(':')[1].trim();
+			return {
+				date: eventsData2[i][0].split('-').join('/'),
+				time: eventsData2[i][1] ? eventsData2[i][1].split(' hs')[0] : 'Sin datos',
+				branch: event[1].split(':')[1].split(' *')[0].trim(),
+				detail: extraDetail
+					? event[0].split('- ')[1] + ' - ' + extraDetail
+					: event[0].split('- ')[1],
+			};
+		})
+		.reverse();
 
 	if (lastEvent) return services.updateResponseHandler(eventsList, lastEvent);
 
